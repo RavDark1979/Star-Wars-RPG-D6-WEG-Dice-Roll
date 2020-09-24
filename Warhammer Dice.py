aedc3b80d5af846420d541e882138bff82d7cd0f
Darklighter79 <66016783+Darklighter79@users.noreply.github.com>
2020-09-24 15:48:09 +0200

    update

---
+++ Warhammer Dice.py
@@ -0,0 +1,16 @@
+import random
+
+n = int(input("Enter your skill value: "))
+
+roll = random.randint(1, 100)
+print(f"Skill value is {n}. You rolled {roll}.")
+
+if roll in range(96, 100) and n < roll:
+    print(
+        f"Critical failure. Test failed by: {roll - n}.That is {roll // n} of your skill")
+elif roll in range(1, 4) and n > roll:
+    print(f"Wow! Critical success! Test beaten by {n - roll}. That is {n // roll} of your skill")
+elif roll <= n:
+    print(f"Sucess! Test beaten by {n - roll}. That is {n // roll} of your skill")
+elif roll > n:
+    print(f"Test failed by: {roll - n}. That is {roll // n} of your skill")
