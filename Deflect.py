edc5a928e32de4961e0a9fd7c42f1d1d5354a73c
RavDark1979 <darklighter1979@gmail.com>
2021-04-19 00:05:46 +0200

    update

---
+++ Deflect.py
@@ -0,0 +1,44 @@
+import random
+
+n = int(input("Enter Sense dice pool: "))
+x = int(input("Enter number of shots: "))
+
+lst_num = []
+
+for _ in range(n):
+    lst_num.append(random.randint(1, 6))
+
+sum_rolls = sum(lst_num)
+
+if lst_num[-1] == 1 and n >= 2:
+    lst_num.remove(min(lst_num))
+    lst_num.remove(max(lst_num))
+    print("Complication - I got a bad feeling about this...")
+
+elif lst_num[-1] == 1 and n == 1:
+    lst_num.remove(min(lst_num))
+    print("Complication - I got a bad feeling about this...")
+
+
+elif lst_num[-1] == 6:
+    while lst_num[-1] == 6:
+        lst_num.append(random.randint(1, 6))
+        print("Advantage gained, but don't get cocky!!")
+
+sum_rolls = sum(lst_num)
+deflect_diff = x * 3
+deflected = sum_rolls // 3
+not_deflected = (deflect_diff - sum_rolls) // 3 + 1
+
+print(f'Rolled: {lst_num}, Total: {sum_rolls}')
+print(f'Total deflect difficulty: {deflect_diff}')
+
+if deflect_diff <= sum_rolls:
+    print("You have deflect all shots")
+
+elif deflect_diff > sum_rolls:
+    print(f'You have deflected {deflected} shot(s).'
+          f'You were hit with {not_deflected} shot(s).')
+
+elif sum_rolls == 0:
+    print(f'You were hit with {deflect_diff / 3} number of shot(s).')
