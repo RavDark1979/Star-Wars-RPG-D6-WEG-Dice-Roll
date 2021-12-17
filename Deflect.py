f541080972284fb3ff4fdbbbde3ab63528676030
Darklighter79 <darklighter1979@gmail.com>
2021-12-17 20:14:40 +0100

    Update

--- Deflect.py
+++ Deflect.py
@@ -1,41 +1,51 @@
-import random
+def deflectroll():
 
-n = int(input("Enter Sense dice pool: "))
-x = int(input("Enter number of shots: "))
+    import random
 
-lst_num = []
+    n = int(input("Enter Sense dice pool: "))
+    x = int(input("Enter number of shots: "))
 
-for _ in range(n):
-    lst_num.append(random.randint(1, 6))
+    lst_num = []
 
-if lst_num[-1] == 1 and n >= 2:
-    lst_num.remove(min(lst_num))
-    lst_num.remove(max(lst_num))
-    print("Complication - I got a bad feeling about this...")
-
-elif lst_num[-1] == 1 and n == 1:
-    lst_num.remove(min(lst_num))
-    print("Complication - I got a bad feeling about this...")
-
-
-elif lst_num[-1] == 6:
-    while lst_num[-1] == 6:
+    for _ in range(n):
         lst_num.append(random.randint(1, 6))
-        print("Advantage gained, but don't get cocky!!")
 
-sum_rolls = sum(lst_num)
+    if lst_num[-1] == 1 and n >= 2:
+        lst_num.remove(min(lst_num))
+        lst_num.remove(max(lst_num))
+        print("Complication - I got a bad feeling about this...")
 
-deflect_diff = (x * 3)
-deflected = sum_rolls // 3
-not_deflected = x
+    elif lst_num[-1] == 1 and n == 1:
+        lst_num.remove(min(lst_num))
+        print("Complication - I got a bad feeling about this...")
 
 
-print(f'Rolled: {lst_num}, Total: {sum_rolls}')
-print(f'Total deflect difficulty: {deflect_diff}')
+    elif lst_num[-1] == 6:
+        while lst_num[-1] == 6:
+            lst_num.append(random.randint(1, 6))
+            print("Advantage gained, but don't get cocky!!")
 
-if deflect_diff <= sum_rolls:
-    print("You have deflect all shots")
+    sum_rolls = sum(lst_num)
 
-elif deflect_diff > sum_rolls:
-    print(f'You have deflected {deflected} shot(s).'
-          f'You were hit with {not_deflected - deflected} shot(s).')
+    deflect_diff = (x * 3)
+    deflected = sum_rolls // 3
+    not_deflected = x
+
+
+    print(f'Rolled: {lst_num}, Total: {sum_rolls}')
+    print(f'Total deflect difficulty: {deflect_diff}')
+
+    if deflect_diff <= sum_rolls:
+        print("You have deflect all shots")
+
+    elif deflect_diff > sum_rolls:
+        print(f'You have deflected {deflected} shot(s).'
+              f'You were hit with {not_deflected - deflected} shot(s).')
+
+    again = input("Again? 'y' to continue  ")
+    if again == "y":
+        deflectroll()
+    else:
+        print("May the Force be with you")
+
+deflectroll()
