4823250eee3d2a63d8d4a059fedf7a16560c4a61
Darklighter79 <darklighter1979@gmail.com>
2021-12-17 20:16:36 +0100

    Update

--- DiceD6.py
+++ DiceD6.py
@@ -1,37 +1,48 @@
-import random
+def skillroll():
 
-n = int(input("Enter dice pool: "))
-x = int(input("Enter test difficulty value: "))
+    import random
 
-lst_num = []
+    n = int(input("Enter dice pool: "))
+    x = int(input("Enter test difficulty value: "))
 
-for _ in range(n):
-    lst_num.append(random.randint(1, 6))
+    lst_num = []
 
-sum_rolls = sum(lst_num)
-
-if lst_num[-1] == 1 and n >= 2:
-    lst_num.remove(min(lst_num))
-    lst_num.remove(max(lst_num))
-    print("Complication - I got a bad feeling about this...")
-
-elif lst_num[-1] == 1 and n == 1:
-    lst_num.remove(min(lst_num))
-    print("Complication - I got a bad feeling about this...")
-
-
-elif lst_num[-1] == 6:
-    while lst_num[-1] == 6:
+    for _ in range(n):
         lst_num.append(random.randint(1, 6))
-        print("Advantage gained, but don't get cocky!!")
 
-sum_rolls = sum(lst_num)
+    sum_rolls = sum(lst_num)
 
-if x > sum_rolls:
-    print("Test failed!")
+    if lst_num[-1] == 1 and n >= 2:
+        lst_num.remove(min(lst_num))
+        lst_num.remove(max(lst_num))
+        print("Complication - I got a bad feeling about this...")
 
-elif x <= sum_rolls:
-    print("Test was sucessful!")
+    elif lst_num[-1] == 1 and n == 1:
+        lst_num.remove(min(lst_num))
+        print("Complication - I got a bad feeling about this...")
 
-print(f'Rolled: {lst_num}')
-print(f'Total: {sum_rolls}')
+
+    elif lst_num[-1] == 6:
+        while lst_num[-1] == 6:
+            lst_num.append(random.randint(1, 6))
+            print("Advantage gained, but don't get cocky!!")
+
+    sum_rolls = sum(lst_num)
+
+    if x > sum_rolls:
+        print("Test failed!")
+
+    elif x <= sum_rolls:
+        print("Test was sucessful!")
+
+    print(f'Rolled: {lst_num}')
+    print(f'Total: {sum_rolls}')
+
+    again = input("Again? 'y' to continue  ")
+    if again == "y":
+        skillroll()
+    else:
+        print("May the Force be with you")
+
+
+skillroll()
